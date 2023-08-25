4888ab9e6b26f1597253db8811f273bf54509a82
DilshadMr360 <86904054+DilshadMr360@users.noreply.github.com>
2023-08-25 19:21:54 +0530

    edit user functionality

--- src/pages/Users/Edit.user.jsx
+++ src/pages/Users/Edit.user.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TextField from '@mui/material/TextField';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 import Admin_header from '../../components/Admin_header';
+import axios from 'axios';
 
 const Edit_User = () => {
-  const [selectedJobRole, setSelectedJobRole] = useState('admin');
+  const [selectedRole, setSelectedRole] = useState('');
   const [fullname, setFullName] = useState('');
   const [jobTitle, setJobTitle] = useState('');
   const [phonenumber, setPhoneNumber] = useState('');
@@ -23,20 +24,71 @@ const Edit_User = () => {
   const [phonenumbererror, setPhoneNumberError] = useState('');
   const [emailerror, setEmailError] = useState('');
   const [passworderror, setPasswordError] = useState('');
-  const [jobTitleError, setJobTitleError] = useState('');
+  const [roleError, setRoleError] = useState('');
   const [confirmpassworderror, setConfirmPasswordError] = useState('');
   const [specializedCountryError, setSpecializedCountryError] = useState('');
   const [qualificationsError, setQualificationsError] = useState('');
   const [jobFieldsError, setJobFieldsError] = useState('');
   const [experienceError, setExperienceError] = useState('');
 
+  //getting the countirs for DB  and storing it as countires
+  // store all the user selected countirs in selected countries
+
+  const [selectedCountries, setSelectedCountries] = useState([]);
+  const [countries, setCountries] = useState([]);
+
+  const [selectedJobs, setSelectedJobs] = useState([]);
+  const [jobs, setJobs] = useState([]);
+
+  const { userId } = useParams();
+
+  useEffect(() => {
+    const headers = {
+        'Authorization': 'Bearer ' + localStorage.getItem('token')
+    };
+
+    axios.get("http://localhost:8000/api/users/" + userId, {headers})
+    .then(response => {
+        let user = response.data.user;
+        setFullName(user.name);
+        SetEmail(user.email);
+        setSelectedRole(user.role); // TODO CHANGE TO SetRole
+        setPhoneNumber(user.phone);
+        setSelectedJobs(response.data.jobs);
+        setSelectedCountries(response.data.countries);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+    axios.get('http://localhost:8000/api/countries')
+      .then(response => {
+        setCountries(response.data.countries);
+    });
+
+    axios.get('http://localhost:8000/api/jobs')
+      .then(response => {
+        setJobs(response.data.jobs);
+      });
+  }, []);
+
+  const handleCountryCheckboxChange = (country) => {
+    if (selectedCountries.includes(country)) {
+      setSelectedCountries(selectedCountries.filter((c) => c !== country));
+    } else {
+      setSelectedCountries([...selectedCountries, country]);
+    }
+  };
+
   const handleRoleChange = (event) => {
-    setSelectedJobRole(event.target.value);
+    setSelectedRole(event.target.value);
+    setSelectedCountries([]);
+    setJobFields('');
   };
 
   const validateSpecializedCountry = () => {
-    if (selectedJobRole === 'consultants' && !specializedCountry) {
-      setSpecializedCountryError('Please enter the specialized country.');
+    if (selectedRole === 'consultants' && selectedCountries.length === 0) {
+      setSpecializedCountryError('Please select a country.');
       return false;
     }
     setSpecializedCountryError('');
@@ -44,7 +96,7 @@ const Edit_User = () => {
   };
 
   const validateJobFields = () => {
-    if ((selectedJobRole === 'consultants' || selectedJobRole === 'jobSeekers') && !jobFields) {
+    if (selectedRole === 'consultants' && !jobFields) {
       setJobFieldsError('Please enter the job fields.');
       return false;
     }
@@ -52,62 +104,57 @@ const Edit_User = () => {
     return true;
   };
 
-  const validateQualifications = () => {
-    if (selectedJobRole === 'jobSeekers' && !qualifications) {
-      setQualificationsError('Please enter your qualifications.');
-      return false;
-    }
-    setQualificationsError('');
-    return true;
-  };
-
-  const validateExperience = () => {
-    if (selectedJobRole === 'jobSeekers' && !experience) {
-      setExperienceError('Please enter your experience.');
-      return false;
-    }
-    setExperienceError('');
-    return true;
-  };
-
-  
-
   const handleSubmit = (e) => {
     e.preventDefault();
 
     let valid = true;
 
-    if (!jobTitle) {
-      setJobTitleError('Please enter your job title.');
-      valid = false;
-    }
-
     if (!fullname) {
       setFullNameError('Please enter your Full name.');
       valid = false;
+    } else {
+      setFullNameError('');
     }
 
     if (!email) {
       setEmailError('Please enter your Email.');
       valid = false;
+    } else {
+      setEmailError('');
     }
 
     if (!phonenumber) {
       setPhoneNumberError('Please enter your Contact Number.');
       valid = false;
-    }
-
-    if (!password) {
-      setPasswordError('Please enter a Password.');
+    } else if (phonenumber.length < 10) {
+      setPhoneNumberError('Phone number must be at least 10 digits.');
       valid = false;
+    } else {
+      setPhoneNumberError('');
     }
 
-    if (!confirmpassword) {
+    if (password && password.length < 8) {
+      setPasswordError('Password must be at least 8 characters.');
+      valid = false;
+    } else {
+      setPasswordError('');
+    }
+
+    if (password && !confirmpassword) {
       setConfirmPasswordError('Please confirm your Password.');
       valid = false;
-    } else if (confirmpassword !== password) {
+    } else if (password && confirmpassword !== password) {
       setConfirmPasswordError('Passwords do not match.');
       valid = false;
+    } else {
+      setConfirmPasswordError('');
+    }
+
+    if (selectedRole.length == 0) {
+      setRoleError('Please select a user role.');
+      valid = false;
+    } else {
+      setRoleError('');
     }
 
     if (!validateSpecializedCountry()) {
@@ -118,16 +165,39 @@ const Edit_User = () => {
       valid = false;
     }
 
-    if (!validateQualifications()) {
-      valid = false;
-    }
-
-    if (!validateExperience()) {
-      valid = false;
-    }
-
     if (valid) {
-      // Your form submission logic here
+      console.log("Updating user ....");
+      const headers = {
+        'Authorization': 'Bearer ' + localStorage.getItem('token')
+      };
+      axios.put('http://localhost:8000/api/users/' + userId, {
+        name: fullname,
+        email: email,
+        phone: phonenumber,
+        role: selectedRole,
+        password: password,
+        password_confirmation: confirmpassword,
+        jobs: selectedJobs,
+        countries: selectedCountries
+      }, { headers })
+        .then(response => {
+          if (response.data.success) {
+            console.log('Post Request Success');
+            localStorage.removeItem('token');
+          }
+        })
+        .catch(error => {
+          let errors = error.response.data.errors;
+          if(errors){
+            setRoleError(errors.role ?? null)
+            setEmailError(errors.email ?? null);
+            setPhoneNumberError(errors.phone ?? null);
+            setFullNameError(errors.name ?? null);
+            setPasswordError(errors.password ?? null);
+          }
+        });
+    } else {
+      console.log("Invalid form");
     }
   };
 
@@ -138,10 +208,10 @@ const Edit_User = () => {
       <div className='bg-purple-300 max-w-full h-screen'>
         <div className="flex items-center justify-center ">
           <div className='container w-full md:w-6/12 mx-auto rounded-lg border-purple-800 bg-white border-2 px-5 py-5 md:mt-5'>
-            <h2 className="text-lg font-semibold mb-4">Edit User</h2>
+            <h2 className="text-lg font-semibold mb-4">Create New User</h2>
             <div className="space-y-4">
               <form onSubmit={handleSubmit}>
-              <div>
+                <div>
                   <label htmlFor="fullname">Full Name</label>
                   <TextField
                     id="fullname"
@@ -179,109 +249,94 @@ const Edit_User = () => {
                   {phonenumbererror && <p className="text-red-500">{phonenumbererror}</p>}
                 </div>
                 <div>
-                  <label htmlFor="jobRole">Job Role:</label>
+                  <label htmlFor="jobRole">User Role:</label>
                   <Select
-                    id="jobRole"
+                    id="role"
+                    name='role'
                     className="border border-gray-300 p-1 w-full"
-                    value={selectedJobRole}
+                    value={selectedRole}
+                    displayEmpty
                     onChange={handleRoleChange}
                   >
+                    <MenuItem disabled value="">
+                      <em>Select a Role</em>
+                    </MenuItem>
                     <MenuItem value="admin">Admin</MenuItem>
-                    <MenuItem value="consultants">Consultants</MenuItem>
-                    <MenuItem value="jobSeekers">Job Seekers</MenuItem>
+                    <MenuItem value="consultant">Consultants</MenuItem>
+                    <MenuItem value="client">Job Seekers</MenuItem>
                   </Select>
                 </div>
+                {roleError && <p className="text-red-500">{roleError}</p>}
+
+
+
+                {selectedRole === 'consultant' && (
+                  <>
+                    <div>
+                      <label htmlFor="specializedCountry">Specialized Country:</label>
+                      <div className="flex flex-wrap gap-1">
+                        <Select
+                          multiple
+                          value={selectedCountries}
+                          onChange={(e) => setSelectedCountries(e.target.value)}
+                        >
+                          {countries.map((country) => (
+                            <MenuItem key={country.id} value={country.id}>
+                              {country.name}
+                            </MenuItem>
+                          ))}
+                        </Select>
+                      </div>
+                      {specializedCountryError && (
+                        <p className="text-red-500">{specializedCountryError}</p>
+                      )}
+                    </div>
+                    <div>
+                      <label htmlFor="jobfields">Job Fieds:</label>
+                      <Select
+                        multiple
+                        value={selectedJobs}
+                        onChange={(e) => setSelectedJobs(e.target.value)}
+                      >
+                        {jobs.map((job) => (
+                          <MenuItem key={job.id} value={job.id}>
+                            {job.name}
+                          </MenuItem>
+                        ))}
+                      </Select>
+                      {jobFieldsError && (
+                        <p className="text-red-500">{jobFieldsError}</p>
+                      )}
+                    </div>
+                  </>
+                )}
+
 
                 <div>
-                      <label htmlFor='password'>Password:</label>
-                      <input
-                        type='password'
-                        id='password'
-                        className='border border-gray-300 p-1 w-full'
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                      />
-                      {passworderror && <p className="text-red-500">{passworderror}</p>}
-                    </div>
-                    <div>
-                      <label htmlFor='confirmpassword'>Confirm Password:</label>
-                      <input
-                        type='password'
-                        id='confirmpassword'
-                        className='border border-gray-300 p-1 w-full'
-                        value={confirmpassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                      />
-                      {confirmpassworderror && <p className="text-red-500">{confirmpassworderror}</p>}
-                    </div>
-            
-                {selectedJobRole === 'consultants' && (
-                  <>
-                    <div>
-                      <label htmlFor='specializedCountry'>Specialized Country:</label>
-                      <input
-                        type='text'
-                        id='specializedCountry'
-                        className='border border-gray-300 p-1 w-full'
-                        value={specializedCountry}
-                        onChange={(e) => setSpecializedCountry(e.target.value)}
-                      />
-                      {specializedCountryError && <p className="text-red-500">{specializedCountryError}</p>}
-                    </div>
-                    <div>
-                      <label htmlFor='jobFields'>Job Fields:</label>
-                      <input
-                        type='text'
-                        id='jobFields'
-                        className='border border-gray-300 p-1 w-full'
-                        value={jobFields}
-                        onChange={(e) => setJobFields(e.target.value)}
-                      />
-                      {jobFieldsError && <p className="text-red-500">{jobFieldsError}</p>}
-                    </div>
-                  </>
-                )}
-                {selectedJobRole === 'jobSeekers' && (
-                  <>
-                    <div>
-                      <label htmlFor='qualifications'>Qualifications:</label>
-                      <input
-                        type='text'
-                        id='qualifications'
-                        className='border border-gray-300 p-1 w-full'
-                        value={qualifications}
-                        onChange={(e) => setQualifications(e.target.value)}
-                      />
-                      {qualificationsError && <p className="text-red-500">{qualificationsError}</p>}
-                    </div>
-                    <div>
-                      <label htmlFor='experience'>Experience:</label>
-                      <input
-                        type='text'
-                        id='experience'
-                        className='border border-gray-300 p-1 w-full'
-                        value={experience}
-                        onChange={(e) => setExperience(e.target.value)}
-                      />
-                      {experienceError && <p className="text-red-500">{experienceError}</p>}
-                    </div>
-                    <div>
-                  <label htmlFor="jobtitle">Job Field:</label>
-                  <TextField
-                    id="jobtitle"
-                    name="jobtitle"
-                    placeholder="Enter Your Job Title"
-                    className="border p-2 rounded-md focus:outline-none focus:border-blue-500 w-full"
-                    value={jobTitle}
-                    onChange={(e) => setJobTitle(e.target.value)}
+                  <label htmlFor='password'>Password:</label>
+                  <input
+                    type='password'
+                    id='password'
+                    className='border border-gray-300 p-1 w-full'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                   />
-                  {jobTitleError && <p className="text-red-500">{jobTitleError}</p>}
+                  {passworderror && <p className="text-red-500">{passworderror}</p>}
                 </div>
-                  </>
-                )}
+                <div>
+                  <label htmlFor='confirmpassword'>Confirm Password:</label>
+                  <input
+                    type='password'
+                    id='confirmpassword'
+                    className='border border-gray-300 p-1 w-full'
+                    value={confirmpassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                  />
+                  {confirmpassworderror && <p className="text-red-500">{confirmpassworderror}</p>}
+                </div>
                 {/* ... other form fields */}
                 <div className='flex flex-row justify-between md:mt-5'>
-                  <Link to={'/users/edit'}>
+                  <Link to={'/users/all'}>
                     <button
                       type="button"
                       className="bg-gray-400 text-white px-4 py-2 rounded-md hover:bg-purple-950 focus:ring focus:ring-blue-300  w-32"
@@ -306,5 +361,4 @@ const Edit_User = () => {
     </>
   );
 }
-
 export default Edit_User;
