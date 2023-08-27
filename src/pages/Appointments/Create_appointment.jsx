f615e6211856284cd3d65efd7c0c462470b9da1f
DilshadMr360 <86904054+DilshadMr360@users.noreply.github.com>
2023-08-27 17:50:42 +0530

    updating states

--- src/pages/Appointments/Create_appointment.jsx
+++ src/pages/Appointments/Create_appointment.jsx
@@ -1,33 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../../components/Header';
 import TextField from '@mui/material/TextField';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import Select from '@mui/material/Select';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import Admin_header from '../../components/Admin_header';
 import MenuItem from '@mui/material/MenuItem';
+import axios from "axios";
+
 
 export default function () {
     const [selectedDate, setSelectedDate] = useState(null);
-    const [selectedCountry, setSelectedCountry] = useState('');
+    const [selectedCountry, setSelectedCountry] = useState(null);
+    const [selectedJob, setSelectedJob] = useState(null);
+    const [email, setEmail] = useState('');
     const [jobTitle, setJobTitle] = useState('');
+    const [countries, setCountries] = useState([]);
+    const [jobs, setJobs] = useState([]);
+    const [selectedconsultant, setSelectedConsultant] = useState(null);
+    const [consultants, setConsultants] = useState([]);
 
-    const [jobTitleError, setJobTitleError] = useState('');
+    const [consultantError, setConsultantError] = useState([]);
+    const [emailError, setEmailError] = useState('');
     const [countryError, setCountryError] = useState('');
+    const [jobError, setJobTitleError] = useState('');
     const [dateError, setDateError] = useState('');
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/countries')
+            .then(response => {
+                setCountries(response.data.countries);
+            });
+
+        axios.get('http://localhost:8000/api/jobs')
+            .then(response => {
+                setJobs(response.data.jobs);
+            });
+
+    }, []);
+
+    useEffect(() => {
+        fetchConsultants();
+
+    }, [selectedJob, selectedCountry]);
 
     const handleDateChange = (date) => {
         setSelectedDate(date);
         setDateError('');
-    };
+    }
 
     const handleCountryChange = (event) => {
         setSelectedCountry(event.target.value);
         setCountryError('');
     };
 
-    const handleJobTitleChange = (e) => {
-        const value = e.target.value;
+    const handleEmailChange = (event) => {
+        const value = event.target.value;
+        setEmail(value);
+        setEmailError('');
+    };
+
+    const handleJobTitleChange = (event) => {
+        const value = event.target.value;
         setJobTitle(value);
         setJobTitleError('');
     };
@@ -37,13 +72,21 @@ export default function () {
 
         let valid = true;
 
-        if (!jobTitle) {
-            setJobTitleError('Please enter your job title.');
+        if (!email) {
+            setEmailError('Please enter your email.');
+            valid = false;
+        } else if (!isValidEmail(email)) {
+            setEmailError('Please enter a valid email address.');
             valid = false;
         }
 
         if (!selectedCountry) {
-            setCountryError('Please select a country.');
+            setCountryError('Select a country.');
+            valid = false;
+        }
+
+        if (!jobTitle) {
+            setJobTitleError('Please enter your job title.');
             valid = false;
         }
 
@@ -52,110 +95,171 @@ export default function () {
             valid = false;
         }
 
-        if (valid) {
-            // Your form submission logic here
-        }
-    };
+        console.log("Creating appointment ....");
+        const headers = {
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+        };
+
+        const appointmentData = {
+            country_id: selectedCountry,
+            job_id: selectedJob,
+            consultant_id: selectedconsultant,
+            time: selectedDate
+        };
+
+        axios.post('http://localhost:8000/api/appointments', appointmentData, { headers })
+            .then(response => {
+                if (response.data.success) {
+                    console.log('Appointment saved successfully');
+                    // Handle success, maybe redirect or show a success message
+                    // Example: navigate("/appointments/all");
+                    localStorage.removeItem('token');
+                }
+            })
+            .catch(error => {
+                console.error('Error saving appointment:', error);
+                // Handle error, maybe show an error message
+            });
+    }
+
+    const isValidEmail = (email) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+
+    const fetchConsultants = () => {
+        axios.get(`http://localhost:8000/api/consultants?job_id=` + selectedJob + `&country_id=` + selectedCountry)
+            .then(response => {
+                console.log(response);
+                setConsultants(response.data.users);
+            });
+    }
+
 
     return (
         <>
+            <Admin_header />
+
             <div className="user-navbar">
-                <Header />
             </div>
 
             <div className="bg-purple-300 min-h-screen flex items-center justify-center">
-                <div className="container w-full md:w-6/12 mx-auto rounded-lg border-purple-800 bg-white border-2 px-5 py-5 ">
+                <div className='container w-full md:w-6/12 mx-auto rounded-lg border-purple-800 bg-white border-2 px-5 py-5 md:mt-3 '>
                     <div>
-                        <h4 className="text-gray-500 pb-2 font-bold inline-block">Book an Appointment</h4>
-                        <hr className="border-1 border-purple-800" />
+                        <h4 className="text-gray-500 pb-2 border-b border-gray-500 font-bold inline-block">Your Upcoming Appointment</h4>
                     </div>
 
                     <form className="flex flex-col space-y-4" onSubmit={handleSubmit}>
-                        <div className="md:flex md:justify-between md:w-full">
-                            <div className="mb-4 md:w-6/12 md:mr-2">
+                        <div className='md:flex md:justify-between md:w-full'>
+                            <div className='mb-4 md:w-6/12 md:mr-2'>
                                 <label htmlFor="jobtitle">Job Field</label>
-                                <TextField
-                                    id="jobtitle"
-                                    name="jobtitle"
-                                    placeholder="Enter Your Job Title"
-                                    className="border p-2 rounded-md focus:outline-none focus:border-blue-500 w-full"
-                                    value={jobTitle}
-                                    onChange={handleJobTitleChange}
-                                />
-                                {jobTitleError && <p className="text-red-500">{jobTitleError}</p>}
-                            </div>
-                        </div>
-
-                        <div className="md:flex md:justify-between md:w-full">
-                            <div className="mb-4 md:w-6/12 md:mr-2">
-                                <label htmlFor="date">Date</label>
-                                <div>
-                                    <DatePicker
-                                        id="Date"
-                                        name="Date"
-                                        selected={selectedDate}
-                                        onChange={handleDateChange}
-                                        placeholderText="Select a date"
-                                        className="border p-2 rounded-md focus:outline-none focus:border-blue-500 w-72 h-[55px]"
-                                        showTimeSelect
-                                        timeFormat="HH:mm"
-                                        timeIntervals={15}
-                                        timeCaption="Time"
-                                        dateFormat="MMMM d, yyyy h:mm aa"
-                                    />
-                                    {dateError && <p className="text-red-500">{dateError}</p>}
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="md:flex md:justify-between md:w-full">
-                            <div className="md:w-6/12 md:ml-2">
-                                <label htmlFor="prefercountry">Country</label>
-
                                 <Select
-                                    id="country"
-                                    name="country"
+                                    value={selectedJob}
+                                    onChange={(e) => setSelectedJob(e.target.value)}
                                     className="border p-2 rounded-md focus:outline-none focus:border-blue-500 w-full h-[56px]"
                                     variant="outlined"
-                                    displayEmpty
-                                    value={selectedCountry}
-                                    onChange={handleCountryChange}
                                 >
-                                    <MenuItem disabled value="">
-                                        <em>Select a country</em>
-                                    </MenuItem>
-                                    <MenuItem value="canada">Canada</MenuItem>
-                                    <MenuItem value="dubai">Dubai</MenuItem>
-                                    <MenuItem value="us">US</MenuItem>
-                                    <MenuItem value="maldives">Maldives</MenuItem>
+                                    {jobs.map((job) => (
+                                        <MenuItem key={job.id} value={job.id}>
+                                            {job.name}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                                {jobError && <p className="text-red-500">{jobError}</p>}
+                            </div>
+
+                            <div className='mb-4 md:w-6/12 md:mr-2'>
+                                <label htmlFor="prefercountry">Country</label>
+                                <Select
+                                    value={selectedCountry}
+                                    onChange={(e) => setSelectedCountry(e.target.value)}
+                                    className="border p-2 rounded-md focus:outline-none focus:border-blue-500 w-full h-[56px]"
+                                    variant="outlined"
+                                >
+                                    {countries.map((country) => (
+                                        <MenuItem key={country.id} value={country.id}>
+                                            {country.name}
+                                        </MenuItem>
+                                    ))}
                                 </Select>
                                 {countryError && <p className="text-red-500">{countryError}</p>}
                             </div>
+
                         </div>
 
-                        <div className="flex flex-row w-full justify-between ">
-                            <div>
-                                <h1 className="text-gray-500">Appointment slots available for selected criteria</h1>
-                            </div>
-                            <div className=""></div>
-                        </div>
+                        <div className='md:flex md:justify-between md:w-full'>
 
-                        <div className="flex flex-row justify-between">
-                            <Link to="/client_dashboard">
-                                <button
-                                    type="button"
-                                    className="bg-gray-400 text-white px-4 py-2 rounded-md hover:bg-purple-950 focus:ring focus:ring-blue-300  w-32"
+                            <div className='mb-4 md:w-6/12 md:mr-2'>
+                                <label htmlFor="jobtitle">Consultant</label>
+                                <Select
+                                    value={selectedconsultant}
+                                    onChange={(e) => setSelectedConsultant(e.target.value)}
+                                    className="border p-2 rounded-md focus:outline-none focus:border-blue-500 w-full h-[56px]"
+                                    variant="outlined"
+                                    disabled={!(selectedCountry && selectedJob)}
                                 >
-                                    Back
-                                </button>
-                            </Link>
+                                    {consultants.length > 0 ? consultants.map((consultant) => (
+                                        <MenuItem key={consultant.id} value={consultant.id}>
+                                            {consultant.name}
+                                        </MenuItem>
+                                    )): 
+                                    <MenuItem key={''} value={''} selected={true}>
+                                        No Consultants available for this criteria
+                                    </MenuItem>
+                                    }
+                                </Select>
+                                {consultantError && <p className="text-red-500">{consultantError}</p>}
+                            </div>
 
-                            <div className="flex">
+
+                            <div className='md:w-6/12 md:ml-2'>
+                                <label htmlFor="appointmentDateTime">Appointment Date & Time</label>
+                                <DatePicker
+                                    id="Date"
+                                    name="Date"
+                                    selected={selectedDate}
+                                    onChange={handleDateChange}
+                                    placeholderText="Select a date"
+                                    className='border p-2 rounded-md focus:outline-none focus:border-blue-500 w-72  h-[55px]'
+                                    showTimeSelect
+                                    timeFormat="HH:mm"
+                                    timeIntervals={15}
+                                    timeCaption="Time"
+                                    dateFormat="MMMM d, yyyy h:mm aa"
+                                    minDate={Date.now()}
+                                />
+                                {dateError && <p className="text-red-500">{dateError}</p>}
+                            </div>
+
+
+
+                        </div>
+
+                        <div className='md:flex md:justify-between md:w-full'>
+
+                        </div>
+
+                        <div>
+                            <h4 className="text-gray-500 pb-2 border-b border-gray-500 font-bold inline-block">Schedule</h4>
+                        </div>
+
+                        <div className='flex flex-row justify-between'>
+                            <div className='flex justify-start'>
+                                <Link to={'/appointments/list'}>
+                                    <button
+                                        type="submit"
+                                        className="bg-purple-800 text-white px-4 py-2 rounded-md hover:bg-purple-950 focus:ring focus:ring-blue-300 ml-auto w-32"
+                                    >
+                                        Back
+                                    </button>
+                                </Link>
+                            </div>
+                            <div className='flex'>
                                 <button
                                     type="submit"
-                                    className="bg-purple-800 text-white px-4 py-2 rounded-md hover:bg-purple-950 focus:ring focus:ring-blue-300 ml-2 w-32"
+                                    className="bg-purple-800 text-white px-4 py-2 rounded-md hover:bg-purple-950 focus:ring focus:ring-blue-300 ml-auto w-32"
                                 >
-                                    Submit
+                                    Save
                                 </button>
                             </div>
                         </div>
@@ -163,5 +267,5 @@ export default function () {
                 </div>
             </div>
         </>
-    );
+    )
 }
